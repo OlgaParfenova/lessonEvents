8ccb0b1218f5c9fc1ec943d4f51ae270a0cf8ce2
Olga Parfenova <work.parfenova@gmail.com>
2023-02-01 16:31:57 +0300

    The onclick listener is added to the script

---
+++ script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const outer = document.querySelector('.outer');
+const middle = document.querySelector('.middle');
+const inner = document.querySelector('.inner');
+
+// console.log(outer);
+// console.log(middle);
+// console.log(inner);
+
+// outer.onclick = () => {alert('This is outer')};
+// middle.onclick = () => {alert('This is middle')};
+// inner.onclick = () => {alert('This is inner')};
